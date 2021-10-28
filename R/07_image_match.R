73c47372f9c2bc4a3060de2c566711183e81fd72
David Wachsmuth <david.wachsmuth@mcgill.ca>
2021-10-28 12:10:10 -0400

    Updated image match script

--- R/07_image_match.R
+++ R/07_image_match.R
@@ -18,9 +18,6 @@
 
 source("R/01_startup.R")
 library(matchr)
-library(foreach)
-library(doParallel)
-doParallel::registerDoParallel()
 library(progressr)
 handlers(global = TRUE)
 
@@ -28,17 +25,17 @@ handlers(global = TRUE)
 # Load previous data ------------------------------------------------------
 
 qload("output/str_raw.qsm", nthreads = availableCores())
-rm(daily, host, exchange_rates)
+rm(daily, daily_all, host, exchange_rates)
 
 
 # Specify location on drive to download photos ----------------------------
 
-dl_location <- "/Volumes/Data 2/Scrape photos/toronto" #PEC
+dl_location <- "/Volumes/Data 2/Scrape photos/pec" #PEC
 
 
-# Get AB image URLs -------------------------------------------------------
+# Get image URLs ----------------------------------------------------------
 
-# Get AB urls
+# Get urls
 ab_urls <-
   property %>%
   mutate(urls = if_else(is.na(ab_image_url), ha_image_url, ab_image_url)) %>%
@@ -70,58 +67,30 @@ if (!dir.exists(paste0(dl_location, "/ab"))) {
 
 # Download images ---------------------------------------------------------
 
-foreach(i = seq_along(ab_urls)) %dopar% {
-  try(download.file(ab_urls[[i]], paste0(
-    dl_location, "/ab/", ab_ids[[i]], "-", seq_along(ab_urls[[i]]), ".jpg")))
-}
+download_images(destination = paste0(dl_location, "/ab/"),
+                path = ab_urls, id = ab_ids)
 
 
 # Get new paths -----------------------------------------------------------
 
 ab_paths <- list.files(paste0(dl_location, "/ab"), full.names = TRUE)
-
 rm(dl_location, ab_urls, ab_ids)
 
 
 # Get signatures ----------------------------------------------------------
 
-# Add to previous sigs
-qload("output/img_sigs_old.qsm")
-
-ab_sigs_new <-
-  create_signature(ab_paths[!ab_paths %in% vctrs::field(ab_sigs, "file")])
-ab_sigs <- c(ab_sigs, ab_sigs_new)
-qsavem(ab_sigs, file = "output/img_sigs.qsm", nthreads = availableCores())
-
+ab_sigs <- create_signature(ab_paths)
+qsave(ab_sigs, file = "output/img_sigs.qs", nthreads = availableCores())
 rm(ab_paths)
-rm(ab_sigs_new)
 
 
 # Match images ------------------------------------------------------------
 
 ab_matrix <- match_signatures(ab_sigs)
 ab_matches <- identify_matches(ab_matrix)
-ab_matches_new <- ab_matches
-
-ab_matches <-
-  ab_matches_new %>%
-  anti_join(ab_matches, by = c("x_sig", "y_sig")) %>%
-  mutate(confirmed = FALSE) %>%
-  bind_rows(ab_matches)
-
-ab_changes <- compare_images(ab_matches)
+ab_changes <- confirm_matches(ab_matches)
 ab_matches <- integrate_changes(ab_matches, ab_changes)
 
-qsavem(ab_matches, file = "output/matches_raw.qsm", nthreads = availableCores())
+qsave(ab_matches, file = "output/matches_raw.qs", nthreads = availableCores())
 rm(ab_matrix)
-
-qload("output/matches_raw_old.qsm", nthreads = 32)
-
-
-
-# Save output -------------------------------------------------------------
-
-qsavem(ab_matches, file = "output/matches_raw.qsm",
-       nthreads = availableCores())
-qsavem(ab_changes, file = "output/match_changes.qsm",
-       nthreads = availableCores())
+qsave(ab_changes, file = "output/match_changes.qs", nthreads = availableCores())
